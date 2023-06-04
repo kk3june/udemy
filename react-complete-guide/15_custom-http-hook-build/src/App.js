c8ffa814236532517f1a85d2bbba8ea546e17227
kk3june <kk3june@gmail.com>
2023-06-05 00:21:40 +0900

    section15 - 201. 사용자 정의 Http 훅 사용하기

--- react-complete-guide/15_custom-http-hook-build/src/App.js
+++ react-complete-guide/15_custom-http-hook-build/src/App.js
@@ -2,38 +2,25 @@ import React, { useEffect, useState } from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
+import useHttp from './hooks/use-http';
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json'
-      );
+  // transformTaasks 함수로 서버로부터 응답으로 전달받은 객체를 프론트엔드에서 필요한 구조와 형태를 가진 객체로 변환
+  // 이 함수는 커스텀 훅이 응답을 받게되면 알아서 호출할 것
+  const transformTasks = (taskObj) => {
+    const loadedTasks = [];
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+      for (const taskKey in taskObj) {
+        loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
       }
 
       setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
-  };
+  }
+
+  // useHttp 는 매개변수를 받기도 하지만 loading 상태, error 상태, sendRequest의 포인터를 return 한다.
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp({url: 'https://react-http-6b4a6.firebaseio.com/tasks.json'}, transformTasks);
 
   useEffect(() => {
     fetchTasks();
